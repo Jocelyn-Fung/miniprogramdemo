74c81159d41253c9fa9d18100803ee43cde89250
Jocelyn-Fung <FengRuli2015@163.com>
2020-02-10 17:52:33 +0800

    收藏功能完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -8,9 +8,10 @@ Page({
    */
   data: {
     goodsDetail: {},
-    goodsId:[],
-    // isCollect:true
+    goodsIdArray: [],
+    isCollect: "icon-shoucang"
   },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -20,20 +21,21 @@ Page({
     //调用页面加载数据
     this.getGoodsDetail(options)
     // 保存商品id
-    let goods_id  = options.goods_id
+    let goods_id = options.goods_id
     // 获取本地数据，如果有id说明是收藏的，高亮星星
     wx.getStorage({
       key: 'goodsIdArray',
-      success: function (res) {
-        console.log(res)
-        res.data.forEach(function (e) {
+      success: res => {
+        // console.log(res)
+        res.data.forEach(e => {
           // console.log(e)
           // 如果本地数组中有匹配项，那么切换类成高亮
           if (e == goods_id) {
-            
+            this.setData({
+              isCollect: "icon-shoucang_gaoliang"
+            })
           }
         })
-
       },
     })
   },
@@ -61,27 +63,73 @@ Page({
       current
     })
   },
-  //处理收藏 
+  //点击收藏，点击取消  
   handleCollect: function() {
-    let {goods_id} = this.data.goodsDetail
+    let {
+      goods_id
+    } = this.data.goodsDetail
+    let goodsId = this.data.goodsIdArray
     // console.log('222',goods_id)
-    let goodsId = this.data.goodsId
-      goodsId.push(goods_id)
-      // console.log(goodsId)
-      wx.setStorage({
-        key: 'goodsIdArray',
-        data: goodsId,
-      })
-      wx.showToast({
-        title: '收藏成功',
-      })
-    
+    // 如果用户的收藏数组中没有任何内容，点击就直接收藏改商品
+    wx.getStorage({
+      key: 'goodsIdArray',
+      // 说明本地还没有这个数组，便创建这个数组，把数据push进去
+      fail: () => {
+        goodsId.push(goods_id)
+        wx.setStorage({
+          key: 'goodsIdArray',
+          data: goodsId,
+        })
+        // console.log(this)
+        this.setData({
+            isCollect: "icon-shoucang_gaoliang"
+          }),
+          wx.showToast({
+            title: '收藏成功！',
+          })
 
-    // 判断，如果在本地中已经有匹配的goods_id,那么再次点击就是删除收藏
-    // if(goods_id){
+      },
+      success: (res) => {
+        // 说明本地已经有数组了,那么遍历数组，有一样id的删除，没有的进行保存
+        // console.log(res.data) //未找到则返回-1
+          if (res.data.indexOf(goods_id)===-1) {
+            res.data.push(goods_id)
+            // console.log(res.data)
+            wx.setStorage({
+              key: 'goodsIdArray',
+              data: res.data,
+            })
+            this.setData({
+                isCollect: "icon-shoucang_gaoliang"
+              }),
+              wx.showToast({
+                title: '收藏成功！',
+              })
+          } else{
+           this.removeByValue(res.data, goods_id);
+           wx.setStorage({
+             key: 'goodsIdArray',
+             data: res.data,
+           })
+            this.setData({
+              isCollect: "icon-shoucang"
+            }),
+              wx.showToast({
+                title: '取消收藏！',
+              })
+          }
+      },
+    })
+  },
 
-    // }
-
+  //删除数组中指定元素 封装函数
+  removeByValue(arr, val) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == val) {
+        arr.splice(i, 1);
+        break;
+      }
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -94,7 +142,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-   
+
 
   },
 
