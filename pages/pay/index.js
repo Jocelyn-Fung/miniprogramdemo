b555264e5dc20941a1fe01a2825edaf5059f2760
Jocelyn-Fung <FengRuli2015@163.com>
2020-02-13 20:50:55 +0800

    开始制作支付页面，完成页面静态布局，数据渲染，获取用户的授权地址功能

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,13 +1,48 @@
-// pages/pay/index.js
+import { getSetting, openSetting, chooseAddress} from '../../utils/async.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    carts:[],
+    address:{}
   },
 
+  // 获取页面数据
+  getPay: function(){
+    let carts = wx.getStorageSync('carts')||[];
+    // console.log(carts)
+    let address = wx.getStorageSync('address')||{};
+    this.setData({
+      carts,
+      address
+    })
+  },
+  // 当没有地址的时候，点击选择地址的事件处理函数
+  getsetting: async function(){
+    // 捕捉各种获取权限中可能出现的异常
+    try{
+      // 通过wx.getSetting 获取当前用户拥有的权限
+      let res = await getSetting();
+      // 判断用户有没有获取当前地址的权限
+      // console.log(res)  //用户授权 scope.address
+      if(!res.authSetting["scope.address"]){
+        await openSetting();  //授权后成功后点击返回就可以进行地址的选择了
+      }
+      // 获取用户的地址
+      let address = await chooseAddress();
+      address.all = address.provinceName +address.cityName+ address.countyName +address.detailInfo;
+      wx.setStorageSync("address", address)
+      this.setData({
+        address
+      })
+
+
+    }catch(e){
+      console.log(e)
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +61,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getPay();
   },
 
   /**
--- utils/async.js
+++ utils/async.js
@@ -1,4 +1,5 @@
 // 使用promise 对微信的接口异步转同步
+// 封装wx.showModal
 export const showModal = (params)=>{
   return new Promise(function(resolve, reject){
     wx.showModal({
@@ -7,6 +8,50 @@ export const showModal = (params)=>{
         resolve(res);
       },
       fail:err=>{
+        reject(err);
+      }
+    })
+  })
+}
+// wx.geSetting()
+// promise  封装  获取用户的当前设置
+export const getSetting = params =>{
+  return new Promise(function(resolve,reject){
+    wx.getSetting({
+      success:(res)=> {
+        // console.log(res.authSetting)
+       resolve(res);
+      },
+      fail:(err)=>{
+        reject(err);
+      }
+    })
+  })
+}
+ 
+// openSetting() 封装    打开用户的设置
+export const openSetting = params=>{
+  return new Promise(function(resolve, reject){
+    wx.openSetting({
+      success:(res)=> {
+        resolve(res);
+        // console.log(res.authSetting)
+      },
+      fail:(err)=>{
+        reject(err);
+      }
+    })
+  })
+}
+
+// 用户的收货地址
+export const chooseAddress = params =>{
+  return new Promise(function(resolve, reject){
+    wx.chooseAddress({
+      success:(res)=>{
+        resolve(res);
+      }, 
+      fail:(err)=>{
         reject(err);
       }
     })
