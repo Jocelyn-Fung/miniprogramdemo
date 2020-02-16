42bfd79867e85693e004751ec3f81b95d75a1a7c
Jocelyn-Fung <FengRuli2015@163.com>
2020-02-16 22:26:34 +0800

    完成所有页面，提交存在问题，报错

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -5,11 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    types:[
-      {
-        index:0,
-        value:"功能问题",
-        isSelect:true
+    types: [{
+        index: 0,
+        value: "功能问题",
+        isSelect: true
       },
       {
         index: 1,
@@ -27,14 +26,14 @@ Page({
         isSelect: false
       }
     ],
-    imgsArray:[], 
-    upload_img_count:0
+    imgsArray: [],
+    upload_img_count: 0
   },
   // 上传图片
-  handleUploadImgs: function(){
+  handleUploadImgs: function() {
     wx.chooseImage({
-      count:9,
-      success: (res) =>{
+      count: 9,
+      success: (res) => {
         // console.log(res)
         this.setData({
           imgsArray: [...this.data.imgsArray, ...res.tempFiles]
@@ -43,67 +42,78 @@ Page({
     })
   },
   // 提交反馈
-  handleSubmit: function(){
-    // console.log(this.data.imgsArray)
-    // 如果是空的，那么不可以提交
-    if (!this.data.imgsArray.length){
-      wx.showToast({
-        title:'请上传传图片',
-        icon:'none'
+  handleSubmit: function() {
+    try {
+      let {
+        imgsArray
+      } = this.data
+      // console.log(this.data.imgsArray)
+      // 如果是空的，那么不可以提交
+      if (!imgsArray.length) {
+        wx.showToast({
+          title: '请上传图片',
+          icon: 'none'
+        })
+        return false;
+      }
+      // 否则就显示下载中
+      wx.showLoading({
+        title: '图片上传中...',
       })
-      return false;
-    }
-    // 否则就显示下载中
-    wx.showLoading({
-      title: '图片上传中...',
-    })
-    // 开始上传图片到云端
-    this.data.imgsArray.forEach((v,i)=>{
-      wx.uploadFile({
-        url: 'https://images.ac.cn/Home/Index/UploadAction/',  //新浪图床
-        filePath: v.path,
-        name: 'file',
-        formData:{},
-        success:res=>{
-          // console.log(res)
-          if(i===this.data.imgsArray.legth -1){
-            wx.hideLoading();
-            this.setData({
-              upload_img_count: i+1
-            })
-            wx.navigateBack({
-              delta:1
-            })
-          }else{
-            this.setData({
-              upload_img_count:i+1
-            })
+      // 开始上传图片到云端
+    
+      imgsArray.forEach((v, i) => {
+        // console.log(v.path)
+        wx.uploadFile({
+          url: 'https://images.ac.cn/Home/Index/UploadAction/', //新浪图床
+          filePath: v.path,
+          name: 'file',
+          formData: {},
+          success: res => {
+            // console.log(res)
+            if (i === imgsArray.legth - 1) {
+              wx.hideLoading();
+              this.setData({
+                upload_img_count: i + 1
+              })
+              wx.navigateBack({
+                delta: 1
+              })
+            } else {
+              this.setData({
+                upload_img_count: i + 1
+              })
+            }
           }
-        }
+        })
       })
-    })
+    } catch(e){
+      console.log(e)
+    }
   },
   // 点击问题类型
-  handlePro:function(e){
+  handlePro: function(e) {
     // console.log()
     let index = e.currentTarget.dataset.index
-      let types = this.data.types
-        types.map(v=>{
-          if (index === v.index) {
-            v.isSelect = !v.isSelect
-            return v;
-          }
-      })
-      this.setData({
-        types
-      })
+    let types = this.data.types
+    types.map(v => {
+      if (index === v.index) {
+        v.isSelect = !v.isSelect
+        return v;
+      }
+    })
+    this.setData({
+      types
+    })
   },
   // 长摁删除图片
-  handleCancel: function(e){
+  handleCancel: function(e) {
     let index = e.currentTarget.dataset.index
     // console.log(index)
     // let {imgsArray} = this.data
-    let {imgsArray}= this.data
+    let {
+      imgsArray
+    } = this.data
     // console.log(imgsArray)
     imgsArray.splice(index, 1)
     this.setData({
@@ -113,56 +123,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
